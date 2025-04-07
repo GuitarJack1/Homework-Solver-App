74a0100195071a3a7cccaff7bcbbf143f6e66f5c
GuitarJack1 <tigerjack07@icloud.com>
2025-04-07 12:44:05 -0500

    Wolfram stuff

--- Homework-Solver/app/solutionModal.tsx
+++ Homework-Solver/app/solutionModal.tsx
@@ -1,29 +1,66 @@
-import {
-  findSolution,
-  processInput,
-  processSolution,
-} from "@/components/functions/getSolution";
+import { findSolution, processInput } from "@/components/functions/getSolution";
 import { router, useLocalSearchParams } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function ModalScreen() {
-  let { extractedText } = useLocalSearchParams<{ extractedText?: string }>();
-  let input;
-  let solution;
+  const { extractedText } = useLocalSearchParams<{ extractedText?: string }>();
+  const [input, setInput] = useState<string | null>(null);
+  const [solution, setSolution] = useState<any>({
+    success: false,
+    pods: [null],
+  });
 
-  if (extractedText) {
-    input = processInput(extractedText);
-    solution = processSolution(findSolution(input));
-  }
+  useEffect(() => {
+    const solve = async () => {
+      if (extractedText) {
+        const processed = processInput(extractedText);
+        setInput(processed);
+        const result = await findSolution(processed);
+        setSolution(result);
+      }
+    };
+
+    solve();
+  }, [extractedText]);
 
   return (
     <View style={styles.modalView}>
       <Text style={styles.text}>
         {extractedText ? "Input: " + input : "Something went wrong"}
       </Text>
-      <Text style={styles.text}>
-        {extractedText ? "Solution: " + solution : "Something went wrong"}
-      </Text>
+      <ScrollView>
+        {solution.success &&
+          solution["pods"].map((pod: any, index: number) => {
+            return (
+              <View
+                key={index}
+                style={{
+                  alignContent: "center",
+                  justifyContent: "center",
+                  marginTop: 20,
+                }}
+              >
+                <Text style={styles.text}>{pod.title}</Text>
+                <Image
+                  src={pod.subpods[0].img.src}
+                  style={{
+                    width: pod.subpods[0].img.width,
+                    height: pod.subpods[0].img.height,
+                    alignSelf: "center",
+                  }}
+                />
+              </View>
+            );
+          })}
+      </ScrollView>
       <Button title="Close" onPress={() => router.back()} />
     </View>
   );
--- Homework-Solver/components/functions/getSolution.ts
+++ Homework-Solver/components/functions/getSolution.ts
@@ -1,13 +1,33 @@
-export function processSolution(input: string): string {
-  return input;
-}
+import { WOLFRAM_API_LINK } from "@/constants/SensitiveData";
+import axios from "axios";
 
 export function processInput(input: string): string {
-  input.replace("+", "plus");
-  input.replace("\\\\", "\\");
+  input = input.trim();
+  input = input.replaceAll("+", "plus");
+  input = input.replaceAll("\\\\", "\\");
+  input = input.replaceAll("\\(", "(");
+  input = input.replaceAll("\\)", ")");
+  input = input.replaceAll("$", "");
   return input;
 }
 
-export function findSolution(input: string): object {
-  return { solution: "x = Skibidi" };
+interface ApiResponse {
+  [key: string]: any;
+}
+
+export type findSolutionReturn = {
+  success: boolean;
+  pods: any[];
+};
+
+export async function findSolution(input: string): Promise<findSolutionReturn> {
+  const axiosResponse: any | undefined = await axios.get(
+    WOLFRAM_API_LINK + input
+  );
+
+  const response = axiosResponse
+    ? axiosResponse["data"]["queryresult"]
+    : { success: "false", pods: [null] };
+
+  return { success: response["success"], pods: response["pods"] };
 }
